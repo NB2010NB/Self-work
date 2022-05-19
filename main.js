35518b7906e28db27b1d2c996e1f314d7e76309a
NB2010NB <natanbalai1@gmail.com>
2022-05-19 13:14:25 +0300

    jj

---
+++ main.js
@@ -0,0 +1,182 @@
+//**********while********** */
+
+// var number = 0
+// while (number < 101){
+//     console.log(number)
+//     number++
+// }
+
+
+// var i = 0
+// var sum = 0
+// var avg = 0
+// while (i < 5) {
+//     sum += +prompt();
+//      avg = sum / 5
+//     / while (i < 51) {
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     } i++
+// }
+//*********array************** */
+// var array = [5, 3, 7, 8, 11]
+// var i = 0
+// for (var sum = 0; sum < array.length; sum++) {
+//     // document.write(array[sum])
+//     i += array[sum]
+// }
+// console.log(i,i/array.length)
+
+// var array = ["dani","avi","nati","dan","asi"]
+// for(var i = 0; i<array.length;i++){
+//     console.log(array[i])
+// }
+
+// var arraynumber1 = [7, 5, 3, 1, 6, 9]
+// var arraynumber2 = [8, 3, 7, 2]
+// function printNumbers(array, number) {
+//     for (i = 0; i < array.length; i++) {
+//         if (array[i] == number) {
+//             console.log("true");
+//             return true
+//         }
+//         else {
+//             i++
+//                 // } 
+//                 // document.write(sum, avg);
+//                 // console.log(avg,sum)
+
+//                 // i = 0
+//                 /
+//                 console.log("false");
+//             return false
+//         }
+//     }
+// }
+// printNumbers(arraynumber1, 10)
+// printNumbers(arraynumber2, 8)
+
+// var array2 = [6, 8, 5, 1, 8, 4]
+// function array(i) {
+//     for (var i = 0; i < array.length; i++)
+//         if (i % 2 == 0){
+//             console.log(i)
+//         }
+// }array()
+
+// function digit() {
+//     var number2 = parseInt(prompt("inter number"))
+//     while (number2 / 10 != 0)
+//         console.log(number2 % 10){
+//         var result = num % 10;
+//         number2++
+//     }
+// } digit()
+
+
+
+
+
+// var myarray = [4,"tt", 8, 2, "avi", "yoni", "dan"]
+// for (var i = 0; i < myarray.length; i++) {
+//     if (myarray[i] * 0 != 0) {
+//         myarray[i] = "not number"
+//     }
+// }
+
+// console.log(myarray)
+
+
+
+
+// function myyString() {
+//     var mystring = String(prompt(""))
+//     for (var i = 0; i < mystring.length; i++) {
+//         console.log(mystring[i])
+//     }
+// } myyString()
+
+// var NEWstring = "hello"
+// function mastring(strFromString) {
+//     var newstring = "";
+//     for (var i = 0; i < strFromString.length; i++) {
+//         newstring += strFromString[i] + i
+//     }
+//     return newstring
+// }
+// mastring(NEWstring);
+// console.log(mastring(NEWstring));
+
+
+
+
+// var sum = 0;
+// function beggi() {
+//     for (var i = 0; i < 2; i++) {
+//         sum += +prompt("nn")
+//     } console.log(sum)
+// } 
+// beggi()
+
+// var str = [3, 4, 5, 1, 6, 9, 8, 6, 6, 2]
+// function number() {
+//     for (var i = 0; i < str.length; i++) {
+//         if (str[i] % 2 == 0) {
+//             console.log(str[i])
+//         }
+//         else {
+//         }
+//     }
+// } number()
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var firstName = [9,93,2,3,95,5,]
+// var carc =[5]
+// function somearraay(firstName, carc) {
+//     for (var i = 0; i < firstName.length; i++) {
+//         if (firstName[i] == carc) {
+//             return firstName
+//         }
+//     }
+//     console.log("no  carc")
+
+// }somearraay()
+
+// function sumara(onearray, one) {  //[7,5,4,5,77,8] 5
+//     for (var i = 0; i < onearray; i++) {
+//         if (onearray[i] == one) {
+//             onearray.splice(i, 1);
+//             i--;
+
+//         }
+//     }
+
+// }
+
+var sumARRAY = prompt()
+function sumarray() {
+    for (var i = 0; i < sumARRAY; i++) {
+        if (sumARRAY == sumARRAY) {
+
+        }
+        if else() {
+
+        }
+        else {
+
+        }
+
+    }
+
+} sumarray()
